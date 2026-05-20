a1052030c8b954ffcd80fbd38d3eb5e8315c8ae4
michalkaleta1987-rgb <michalkaleta1987@gmail.com>
2026-05-20 13:44:13 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,4 @@
-const CACHE_NAME = 'trustcall-v1';
-const urlsToCache = [
-  '/Trustcall/',
-  '/Trustcall/index.html',
-  '/Trustcall/manifest.json'
-];
-
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .catch(err => console.log('Cache failed:', err))
-  );
   self.skipWaiting();
 });
 
@@ -19,9 +7,5 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-      .catch(() => caches.match('/Trustcall/'))
-  );
+  // Pozwól wszystkim żądaniom iść normalnie przez sieć
 });
